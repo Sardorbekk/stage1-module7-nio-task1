194c1afc04ed5fef71d8e2572f14462c612e5eea
Sardorbekk <sardorbekimomaddinov@gmail.com>
2023-03-14 18:08:01 +0500

    HI

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,12 +1,10 @@
 package com.epam.mjc.nio;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 public class FileReader {
     public Profile getDataFromFile(File file) {
-        try(java.io.FileReader fileReader=new java.io.FileReader(file);
-            BufferedReader bufferedReader=new BufferedReader(fileReader)) {
+        try(BufferedReader bufferedReader=new BufferedReader(new java.io.FileReader(file))) {
             String nameLine = bufferedReader.readLine();
             String ageLine = bufferedReader.readLine();
             String emailLine = bufferedReader.readLine();
@@ -16,9 +14,6 @@ public class FileReader {
             String email = emailLine.split(": ")[1];
             Long phone = Long.parseLong(phoneLine.split(": ")[1]);
             return new Profile(name,age,email,phone);
-        } catch (FileNotFoundException e) {
-            System.out.println("404 not found");
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }return new Profile();}}
